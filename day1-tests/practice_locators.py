0e8a90c90d30e96bc2a742c425e5630b5c5ec312
anjorpatil16-creator <anjorpatil16@gmail.com>
2025-10-23 19:24:26 +0530

    Add files to day1-tests

---
+++ day1-tests/practice_locators.py
@@ -0,0 +1,77 @@
+#Author: Anjor Patil
+#Date: 06-10-2025
+#Time: 11.20
+#Purpose: Automate login on demo website using and learn to find elements on any page
+
+"""
+Automates login page element detection on https://www.saucedemo.com
+using Selenium. Demonstrates locating elements by ID, NAME,
+CLASS_NAME, CSS_SELECTOR, and XPATH. Requires Selenium and ChromeDriver.
+"""
+
+
+#Import required modules and libraries
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+import time
+
+#Setup
+service = Service(r"C:/chromedriver/chromedriver-win64/chromedriver.exe")
+driver = webdriver.Chrome(service=service)
+
+try:
+    #Open browser with given website URL
+    driver.get("https://www.saucedemo.com")
+    print("Website opened successfully")
+    time.sleep(3)
+    
+    #Different ways to find elements
+    print("Finding elements using different methods")
+    
+    #By ID
+    try:
+        element = driver.find_element(By.ID, "user-name")
+        print("✓ Found by ID")
+    except:
+        print("✗ Not found by ID")
+            
+    #By NAME
+    try:
+        element = driver.find_element(By.NAME, "user-name")
+        print("✓ Found by NAME")
+        
+    except:    
+        print("✗ Not found by NAME")
+        
+    #By CLASS_NAME
+    try:
+        element = driver.find_element(By.CLASS_NAME, "login_logo")
+        print("✓ Found by CLASS_NAME")
+        
+    except:    
+        print("✗ Not found by CLASS_NAME")
+        
+    #By CSS_SELECTOR
+    try:
+        element = driver.find_element(By.CSS_SELECTOR, "#user-name")
+        print("✓ Found by CSS_SELECTOR")
+        
+    except:    
+        print("✗ Not found by CSS_SELECTOR")
+        
+    #By XPATH
+    try:
+        element = driver.find_element(By.XPATH, "//input[@placeholder = 'Password']")
+        print("✓ Found by XPATH")
+        
+    except:    
+        print("✗ Not found by XPATH")
+    
+
+except Exception as e:
+    print(f" x Error: {e}")
+    
+finally: 
+    time.sleep(4)   
+    driver.quit()
